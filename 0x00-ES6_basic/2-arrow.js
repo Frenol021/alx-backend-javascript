1b00c1059987bb5b8326c3be4a63f9dedd899063
frenol021 <frenol021@gmail.com>
2023-07-01 08:17:31 +0000

    arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,8 +1,8 @@
-export default function getNeighborhoodsList() {
+export default getNeighborhoodsList() => {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = add(newNeighborhood) {
+  this.addNeighborhood = add(newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
